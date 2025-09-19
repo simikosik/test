bfbf48bbb1413bea5e2b35bded36b3d1678edebf
simikosik <kosiksimi5@gmail.com>
2025-09-19 12:02:52 +0200

    h

---
+++ popo2/src/app/quest/quest.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quest',
+  imports: [],
+  templateUrl: './quest.html',
+  styleUrl: './quest.css'
+})
+export class Quest {
+
+}
