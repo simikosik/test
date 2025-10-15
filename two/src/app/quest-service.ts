b04bd5a853742631d9039f5fba417f3d12822785
simikosik <kosiksimi5@gmail.com>
2025-10-15 22:18:27 +0200

    9

---
+++ two/src/app/quest-service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { QuestInterface } from './quest-interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuestService {
+
+  private quest1: QuestInterface = {
+      id: 1, title: "Rano vstanem", description: "Najdi zmysel zivota", xp: 10
+  
+    }
+    private quest2: QuestInterface = {
+      id: 2, title: "mikne ma", description: "Vyjeb sa na to", xp: 100
+  
+    }
+    private quest3: QuestInterface = {
+      id: 3, title: "pozriem sa zlava do prava", description: "Choj Spat", xp: 1000
+  
+    }
+    private quests = [this.quest1, this.quest2, this.quest3];
+
+    getQuests(){
+      return [...this.quests]};
+}
--- two/src/app/quests/quests.ts
+++ two/src/app/quests/quests.ts
@@ -1,38 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { QuestInterface } from '../quest-interface';
 import { QuestItem } from '../quest-item/quest-item';
+import { QuestService } from '../quest-service';
 @Component({
   selector: 'app-quests',
   imports: [QuestItem],
   templateUrl: './quests.html',
-  styleUrl: './quests.css'
+  styleUrl: './quests.css',
+  standalone: true
 })
 export class Quests {
-
-  quest1: QuestInterface = {
-    id: 1, title: "Rano vstanem", description: "Najdi zmysel zivota", xp: 10
-
-  }
-  quest2: QuestInterface = {
-    id: 2, title: "mikne ma", description: "Vyjeb sa na to", xp: 100
-
-  }
-  quest3: QuestInterface = {
-    id: 3, title: "pozriem sa zlava do prava", description: "Choj Spat", xp: 1000
-
-  }
-  quests = [this.quest1, this.quest2, this.quest3];
+  questService = inject(QuestService);
+  quests = this.questService.getQuests();
+  
+  
 
   addQuest() {
-    var newId = this.quests.length;
+   var newId = this.quests.length + 1;
 
     const newquest: QuestInterface = {
-      id: newId + 1, title: "Nahanaju ma bestie", description: "Repeat", xp: 10
+      id: newId, title: "Nahanaju ma bestie", description: "Repeat", xp: 10
     }
     this.quests.push(newquest)
   }
   removeQuest(id: number) {
-     this.quests = this.quests.filter(q => q.id !== id);
+    this.quests = this.quests.filter(q => q.id !== id);
   }
 }
 
